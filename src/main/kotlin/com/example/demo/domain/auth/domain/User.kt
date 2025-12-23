f0bd35f7abd6148a3ae8d5412a71cabf4402ed43
황정빈 <hjbin1211@gmail.com>
2025-12-23 09:32:52 +0900

    feat: Add User entity code

--- src/main/kotlin/com/example/demo/domain/auth/domain/User.kt
+++ src/main/kotlin/com/example/demo/domain/auth/domain/User.kt
@@ -1,7 +1,24 @@
 package com.example.demo.domain.auth.domain
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
-class User {
+@Table(name = "users")
+class User (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "u_id")
+    var userId: Long? = null,
+    @Column(name = "u_username", nullable = false)
+    var username: String,
+    @Column(name = "u_name", nullable = false)
+    var userName: String,
+    @Column(name = "u_password", nullable = false)
+    var userPassword: String
+) {
 }
